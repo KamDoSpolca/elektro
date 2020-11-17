3e891caa26947ce0c8c9cb0013e995c889acf09f
KamDoSpolca <martinqo277@azet.sk>
2020-11-17 15:26:58 -0100

    project buildup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StoresComponent } from './stores/stores.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegistrationComponent } from './authentication/registration/registration.component';
+import { UserComponent } from './user/user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'stores', component: StoresComponent },
+  { path: 'register', component: RegistrationComponent },
+  { path: 'userinfo', component: UserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegistrationComponent } from './authentication/registration/registration.component';
+import { UserComponent } from './user/user.component';
+import { StoresComponent } from './stores/stores.component';
+import { MenuComponent } from './_menu/_menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    UserComponent,
+    StoresComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
