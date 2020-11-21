e90f8c6b9b8697779704f808ee89f2edb59c7fd5
Diana Trubirohová <xtrubiro@mendelu.cz>
2020-11-21 21:29:23 +0100

    edit product

--- src/app/products/edit/edit.component.ts
+++ src/app/products/edit/edit.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/products/product.model';
+import { StoreService } from '../../store/store.service';
 
 @Component({
   selector: 'edit',
@@ -8,7 +10,16 @@ import { Component } from '@angular/core';
 export class EditComponent {
   title = 'elektro';
 
+  products: Product[] = [];
+  constructor(
+    protected _storeService: StoreService
+  ) { }
+
+  ngOnInit() {
+    this.products = this._storeService.getMyStoreData()
+  }
+
   onSubmit() {
-    
+
   }
 }
